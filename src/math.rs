be1600990c087e4100ee087bd5693e17a531c76c
Ai, Zi-Xun <aiz@uci.edu>
2022-07-10 14:52:24 +0100

    Add Factorial, Permutation, and Combination functions

--- src/math.rs
+++ src/math.rs
@@ -11,3 +11,28 @@ pub fn gcd(a: i32, b: i32) -> i32 {
 pub fn lcm(a: i32, b: i32) -> i32 {
     a * b / gcd(a, b)
 }
+
+// Factorial: n!
+pub fn factorial(n: i32) -> i32 {
+    (1..=n).product()
+}
+
+// The number of permutation: k-permutations of n
+//  - n: a set size of n
+//  - k: taken k of elements from the set
+// nPk = n! / (n - k)!
+pub fn perm(n: i32, k: i32) -> i32 {
+    (n - k + 1..=n).product()
+}
+
+// The number of combination: k-combination of n
+//  - n: a set size of n
+//  - k: taken k of elements from the set
+// nCk = n! / ((n - k)! * k!)
+pub fn comb(n: i32, k: i32) -> i32 {
+    if k > n {
+        0
+    } else {
+        (1..=k).fold(1, |acc, val| acc * (n - val + 1) / val)
+    }
+}
